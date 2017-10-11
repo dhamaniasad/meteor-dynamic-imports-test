1833d4c59965f9e3dc98b1f2eddb055de13f44cc
Asad Dhamani <dhamaniasad@gmail.com>
2017-10-11 17:34:49 +0530

    Add service worker

--- app/client/main.js
+++ app/client/main.js
@@ -2,3 +2,9 @@
 
 import '/imports/startup/client';
 import '/imports/startup/both';
+
+
+Meteor.startup(() => {
+	navigator.serviceWorker.register('/sw.js').then()
+	.catch(error => console.log('ServiceWorker registration failed: ', err)); 
+});
